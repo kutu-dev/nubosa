ac68401b61dba67544526d8b709e97a5a2e0f3dc
Kutu <git@dobon.com>
2025-04-05 13:16:06 +0200

    Add Bash version check

--- scripts/switch.bash
+++ scripts/switch.bash
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+
+if [ "${BASH_VERSINFO:-0}" -lt 4 ]; then
+    echo "This version of Bash is too old, please update it to at least 4.0.0"
+    exit 1
+fi
+
 set -Eeuo pipefail
 shopt -u nullglob
 shopt -s globstar
